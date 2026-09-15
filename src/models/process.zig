c97f0a7919e65bcf51d31347955bae3ea3c79f80
lizbh26 <lizbh26@proton.me>
2026-09-15 15:42:54 -0600

    feat: added arrival, start and end timestamps to process

--- src/models/process.zig
+++ src/models/process.zig
@@ -1,20 +1,27 @@
 const std = @import("std");
+const zeit = @import("zeit");
+
 const Operation = @import("operation.zig");
 
 pub const Process = struct {
     id: usize,
-    batchIdx: usize,
 
     operation: Operation.Operation,
     tme_ms: i128,
     tt_ms: i128 = 0,
 
-    pub fn seed(self: *Process, random: std.Random, data: struct { id: usize, batchIdx: usize }) void {
+    arrival_time: zeit.Instant,
+    starting_time: ?zeit.Instant,
+    finalization_time: zeit.Instant,
+
+    pub fn seed(self: *Process, random: std.Random, data: struct { id: usize }) void {
         self.id = data.id;
-        self.batchIdx = data.batchIdx;
         self.operation.seed(random);
+
         self.tme_ms = random.intRangeAtMost(i128, 5, 20) * 1000;
         self.tt_ms = 0;
+
+        self.starting_time = null;
     }
 
     pub fn isDone(self: *const Process) bool {
