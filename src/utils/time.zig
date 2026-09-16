59f8ec4c0fbfd51f2df7638b927843e0ee0c044c
lizbh26 <lizbh26@proton.me>
2026-09-16 17:32:52 -0600

    refactor: extracted time to string utility from timer.zig

---
+++ src/utils/time.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const zeit = @import("zeit");
+
+const leftpad = @import("leftpad.zig");
+
+fn time_to_string(alloc: std.mem.Allocator, diff: zeit.Time) ![]const u8 {
+    //IMPORTANT: if simulation goes beyond a day, this will loop back around.
+    var localArena = std.heap.ArenaAllocator.init(alloc);
+    defer localArena.deinit();
+    const localAlloc = localArena.allocator();
+
+    const hours = if (diff.hour > 0) try std.fmt.allocPrint(localAlloc, "{d}:", .{diff.hour}) else "";
+
+    var minutes: []const u8 = try std.fmt.allocPrint(localAlloc, "{d}:", .{diff.minute});
+    if (minutes.len == 2)
+        minutes = leftpad(minutes, 1, '0', localAlloc);
+
+    var seconds: []const u8 = try std.fmt.allocPrint(localAlloc, "{d}.", .{diff.second});
+    if (seconds.len == 2)
+        seconds = leftpad(seconds, 1, '0', localAlloc);
+
+    var milliseconds: []const u8 = try std.fmt.allocPrint(localAlloc, "{d}", .{diff.millisecond});
+    if (milliseconds.len < 3)
+        milliseconds = leftpad(milliseconds, 3 - milliseconds.len, '0', localAlloc);
+
+    return try std.mem.concat(alloc, u8, &.{ hours, minutes, seconds, milliseconds });
+}
--- src/view/processor/header/timer.zig
+++ src/view/processor/header/timer.zig
@@ -10,7 +10,7 @@ const Process = @import("~").models.Process.Process;
 const ExecutionContext = @import("~").models.Context.ExecutionContext;
 
 const usize_to = @import("~").utils.usize_to;
-const leftpad = @import("~").utils.leftpad;
+const time_to_string = @import("~").utils.time_to_string;
 
 pub const TimerWidget = struct {
     alloc: std.mem.Allocator,
@@ -34,30 +34,7 @@ pub const TimerWidget = struct {
     }
     pub fn draw(self: *TimerWidget, win: Window) !void {
         const diff = zeit.instant(.{ .unix_nano = self.ctx.time_ellapsed_nano }, &zeit.utc).time();
-        try self.label.changeText(try self.diffToString(diff));
+        try self.label.changeText(try time_to_string(self.alloc, diff));
         self.label.draw(win);
-    }
-
-    fn diffToString(self: *TimerWidget, diff: zeit.Time) ![]const u8 {
-        //IMPORTANT: if simulation goes beyond a day, this will loop back around.
-        var localArena = std.heap.ArenaAllocator.init(self.alloc);
-        defer localArena.deinit();
-        const localAlloc = localArena.allocator();
-
-        const hours = if (diff.hour > 0) try std.fmt.allocPrint(localAlloc, "{d}:", .{diff.hour}) else "";
-
-        var minutes: []const u8 = try std.fmt.allocPrint(self.alloc, "{d}:", .{diff.minute});
-        if (minutes.len == 2)
-            minutes = leftpad(minutes, 1, '0', self.alloc);
-
-        var seconds: []const u8 = try std.fmt.allocPrint(self.alloc, "{d}.", .{diff.second});
-        if (seconds.len == 2)
-            seconds = leftpad(seconds, 1, '0', self.alloc);
-
-        var milliseconds: []const u8 = try std.fmt.allocPrint(self.alloc, "{d}", .{diff.millisecond});
-        if (milliseconds.len < 3)
-            milliseconds = leftpad(milliseconds, 3 - milliseconds.len, '0', self.alloc);
-
-        return try std.mem.concat(self.alloc, u8, &.{ hours, minutes, seconds, milliseconds });
     }
 };
